2293c1074dab40ab5ea36255e38957913a60efe0
DarkWolf <DarkWolf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-02 15:22:50 +0000

    New Release
    git-svn-id: https://plugins.svn.wordpress.org/my-custom-css/trunk@662487 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- my-custom-css.php
+++ my-custom-css.php
@@ -6,26 +6,30 @@ Description: With this plugin you can put custom css code without edit your them
 It contain also a syntax color and tab support for write a good css code.
 You can see in action (source code) here: http://vegamami.altervista.org/ :)
 Author: Salvatore Noschese - DarkWolf
-Version: 0.8
+Version: 0.9
 Author URI: http://www.darkwolf.it/
 Text Domain: mccss
 */
 
 // Prevent Direct Access with homepage redirect
-if (!defined('DB_NAME')) {
+if (!defined('DB_NAME'))
+{
 	header('Location: ../../../');
 }
 
-function add_my_custom_css() {
+function add_my_custom_css()
+{
 	// Strip tag: Remove html tag from page! Just for security ;)
 	$mycustomcss = strip_tags(get_option('my_custom_css'));
-	if (!empty($mycustomcss)) {
+	if (!empty($mycustomcss))
+	{
 		echo "\n<!-- My Custom CSS Start -->\n<style type=\"text/css\">\n/* Plugin Author: Salvatore Noschese\nDarkWolf: http://www.darkwolf.it/ */\n\n".$mycustomcss."\n</style>\n<!-- My Custom CSS End -->\n";
 	}
 }
 
 // Start Add link on plugins page
-function my_custom_css_links($links) { 
+function my_custom_css_links($links)
+{ 
 	# Settings:
 	$settings_link = '<a href="admin.php?page=my_custom_css" title="My Custom CSS">'.__('Settings','mccss').'</a>';
 	array_unshift($links, $settings_link);
@@ -37,14 +41,17 @@ function my_custom_css_links($links) {
 }
 // End Add link on plugins page
 
-function mccss_admin() {
-	$icon_url = WP_PLUGIN_URL."/".str_replace(basename( __FILE__),"",plugin_basename(__FILE__))."css-icon.png";
+function mccss_admin()
+{
+	$icon_url = WP_PLUGIN_URL."/".str_replace(basename(__FILE__),"",plugin_basename(__FILE__))."css-icon.png";
 	$plugin_page = add_menu_page(__('My Custom CSS Panel','mccss'),__('My Custom CSS','mccss'), 'manage_options', 'my_custom_css', 'mccss_options', $icon_url, 61);
-	add_action( 'admin_init', 'register_settings_mccss' );
-	add_action( 'admin_head-'. $plugin_page, 'mccss_syntax' );
+	add_action('admin_init', 'register_settings_mccss');
+	add_action('admin_head-'. $plugin_page, 'mccss_syntax');
 	// Disable "WP Editor" in this page if is active: http://wordpress.org/extend/plugins/wp-editor/
-	If ( is_plugin_active("wp-editor/wpeditor.php") && $_SERVER['QUERY_STRING'] == 'page=my_custom_css') {
-		function remove_wpeditor_header_info() {
+	If (is_plugin_active("wp-editor/wpeditor.php") && $_SERVER['QUERY_STRING'] == 'page=my_custom_css')
+	{
+		function remove_wpeditor_header_info()
+		{
 		// Wp Editor Style
 		wp_deregister_style('wpeditor');
 		wp_deregister_style('fancybox');
@@ -70,21 +77,25 @@ function mccss_admin() {
 	}
 }
 
-function mccss_syntax() { ?>
+function mccss_syntax()
+{
+?>
 <!-- Syntax Support Start -->
-<link type="text/css" rel="stylesheet" href="<?php echo WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)); ?>syntax/codemirror.css" />
-<script language="javascript" src="<?php echo WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)); ?>syntax/codemirror.js"></script>
-<script language="javascript" src="<?php echo WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)); ?>syntax/css.js"></script>
+<link type="text/css" rel="stylesheet" href="<?php echo WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__)); ?>syntax/codemirror.css" />
+<script language="javascript" src="<?php echo WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__)); ?>syntax/codemirror.js"></script>
+<script language="javascript" src="<?php echo WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__)); ?>syntax/css.js"></script>
 <!-- Syntax Support End -->
-<?php }
+<?php
+}
 
 // Register Settings
-function register_settings_mccss() {
+function register_settings_mccss()
+{
 	register_setting('mccss_settings','my_custom_css');
 }
 
-function mccss_options() {
-
+function mccss_options()
+{
 ?>
 <div class="wrap">
 	<h2><?php _e('My Custom CSS Options','mccss'); ?></h2>
@@ -94,16 +105,16 @@ function mccss_options() {
 	<textarea name="my_custom_css" id="my_custom_css" dir="ltr" style="width:100%;height:350px;"><?php echo get_option('my_custom_css'); ?></textarea>
 	<script language="javascript">var editor = CodeMirror.fromTextArea(document.getElementById("my_custom_css"), { lineNumbers: true });</script>
 	<p class="submit">
-    	<input type="submit" class="button-primary" value="<?php _e('Save','mccss'); ?>" />
-   	</p>
+		<input type="submit" class="button-primary" value="<?php _e('Save','mccss'); ?>" />
+	</p>
 	</form>
 </div>
-<?php 
+<?php
 }
 
 add_action('admin_menu', 'mccss_admin');
-add_action('wp_head', 'add_my_custom_css');
-add_filter("plugin_action_links_".plugin_basename(__FILE__)."", 'my_custom_css_links' );
-load_plugin_textdomain('mccss', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+add_action('wp_head', 'add_my_custom_css', 999);
+add_filter("plugin_action_links_".plugin_basename(__FILE__)."", 'my_custom_css_links');
+load_plugin_textdomain('mccss', false, dirname(plugin_basename(__FILE__)) . '/lang/');
 
 ?>
